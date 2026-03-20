a0d9245087cfdda5da021ee522a6e9a011d1428b
Omar <omar.tourabi7@gmail.com>
2026-03-20 18:01:39 +0100

    rm unused files

--- 1_start/d01/ex01/numbers.py
+++
@@ -1,9 +0,0 @@
-def print_numbers():
-    with open('numbers.txt', 'r') as f:
-        content = f.read().strip()
-        numbers = content.split(',')
-        for number in numbers:
-            print(number.strip())
-
-if __name__ == '__main__':
-    print_numbers()
